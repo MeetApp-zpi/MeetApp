91970d0e0c4bb3e0342da33f850db52aa460e4e7
sebastian.lakomy <sebastian.lakomy@infor.com>
2022-10-10 18:01:47 +0200

    Initialise Spring Boot app

---
+++ backend/meetapp/src/test/java/com/meetapp/meetapp/MeetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetapp.meetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
