b19ac40b640cd54f676df2f514d197394a81c984
sebastian.lakomy <sebastian.lakomy@infor.com>
2022-10-11 09:38:29 +0200

    Configure the Spring Boot PostgreSQL connection

--- backend/meetapp/src/main/java/com/meetapp/meetapp/MeetAppApplication.java
+++ backend/meetapp/src/main/java/com/meetapp/meetapp/MeetAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeetAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MeetAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MeetAppApplication.class, args);
+    }
 }
