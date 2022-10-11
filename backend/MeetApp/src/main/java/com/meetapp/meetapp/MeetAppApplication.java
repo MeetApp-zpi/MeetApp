8fcea8cc26ca95941e40308e3888619d97db50b7
Mendrejk <bastian.lakomy@gmail.com>
2022-10-11 15:55:22 +0200

    Initialise Spring Boot app with Java 17

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/MeetAppApplication.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/MeetAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeetAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeetAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeetAppApplication.class, args);
+    }
 
 }
