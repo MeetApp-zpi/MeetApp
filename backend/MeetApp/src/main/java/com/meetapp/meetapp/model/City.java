f0cb429a58989b6516531e3e544233bd6eb31c12
sebastian.lakomy <sebastian.lakomy@infor.com>
2022-10-13 19:43:28 +0200

    Add the location entity

---
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/model/City.java
@@ -0,0 +1,15 @@
+package com.meetapp.meetapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "city_generator")
+    @SequenceGenerator(name = "city_generator", sequenceName = "city_sequence", allocationSize = 1)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+}
