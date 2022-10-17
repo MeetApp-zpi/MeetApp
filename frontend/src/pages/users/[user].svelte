540cec01c30b56ad8713c9308ee85854c67991c7
Kajetan Pynka <siema1234@gazeta.pl>
2022-10-17 16:58:40 +0200

    Transformed SvelteKit into Svelte with Routify, added built app to Spring Boot's resources folder, added a simple controller to serve svelte app via sSpring Boot (Tomcat server)

---
+++ frontend/src/pages/users/[user].svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let user: String;
+    let data = '';
+
+    $: {
+        fetch(`http://localhost:8080/api/users/${user}`)
+            .then((res) => res.text())
+            .then((res) => (data = res))
+            .catch((err) => console.error(err));
+    }
+</script>
+
+<h1>
+    Welcome {data}
+</h1>
