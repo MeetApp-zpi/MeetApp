96cc820c9958a743718f92e9a2135cc9d33af011
Kajetan Pynka <siema1234@gazeta.pl>
2022-10-25 14:36:07 +0200

    Created a way to retrieve user's attributes from his token (when authorization passed)

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityConfiguration.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityConfiguration.java
@@ -10,17 +10,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(a -> a.antMatchers("/", "/assets/*")
+        http.authorizeRequests(a -> a.antMatchers("/", "/assets/*", "/login")
                     .permitAll().anyRequest().authenticated())
                 .exceptionHandling(e -> e.authenticationEntryPoint(
                         new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
-                )).csrf().disable();
+                ))
+                .oauth2Login()
+                    .defaultSuccessUrl("/loginSuccess", true)
+                .and()
+                .csrf().disable();
 
         return http.build();
     }
