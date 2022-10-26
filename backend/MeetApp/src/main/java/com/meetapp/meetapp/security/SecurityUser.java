04dd398fcd3104da71d53de1505fe0e271257154
KyattPL <siema1234@gazeta.pl>
2022-10-26 22:08:46 +0200

    Given a valid JWT HttpSession is being set as authenticated

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityUser.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/security/SecurityUser.java
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,10 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -54,11 +60,21 @@ public class SecurityUser {
     @GetMapping("/verifyTokenBearer")
     public String verifyTokenBearer(HttpSession sess, HttpServletRequest request) throws GeneralSecurityException, IOException {
         String cookie = request.getHeader("Authorization");
-        if (JwtValidator.verify(cookie)) {
+        String onlyToken = cookie.split(" ")[1];
+        if (JwtValidator.verify(onlyToken)) {
             SecurityContextImpl cont = new SecurityContextImpl();
-            OidcIdToken oidcIdToken = new OidcIdToken("12123");
-            DefaultOidcUser defaultUser = new DefaultOidcUser();
-            OAuth2AuthenticationToken oAuthToken = new OAuth2AuthenticationToken();
+            HashMap<String, Object> claims = new HashMap<>();
+            claims.put("at_hash", "ZDE42342434");
+            claims.put("sub", "4234141414");
+            claims.put("given_name", "Kajetan");
+            claims.put("family_name", "Pynka");
+            claims.put("email", "kyattpoland@gmail.com");
+            OidcIdToken oidcIdToken = new OidcIdToken(onlyToken, Instant.now(), Instant.MAX, claims);
+            OidcUserAuthority oidcUserAuthority = new OidcUserAuthority(oidcIdToken);
+            ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(oidcUserAuthority);
+            DefaultOidcUser defaultUser = new DefaultOidcUser(authorities, oidcIdToken);
+            OAuth2AuthenticationToken oAuthToken = new OAuth2AuthenticationToken(defaultUser, authorities, "google");
             oAuthToken.setAuthenticated(true);
             cont.setAuthentication(oAuthToken);
             sess.setAttribute("SPRING_SECURITY_CONTEXT", cont);
