e661e145afb17be5e1fd9aa8b2e4be67a7dc78e4
KyattPL <siema1234@gazeta.pl>
2022-11-03 20:17:39 +0100

    Implemented button, added default font 'Montserrat'

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+    import Button from '../lib/Button/Button.svelte';
+</script>
+
 <h2>index</h2>
 <a href="/users" target="_self">click link</a>
+
+<Button clickHandler={() => console.log('works')}>Zapisuję się!</Button>
