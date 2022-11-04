a382892e5c49f6b2d4b865c06eeafc73ec46c337
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-04 13:51:21 +0100

    Implemented responsive header

---
+++ frontend/src/lib/Header/Header.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import MdMenu from 'svelte-icons/md/MdMenu.svelte';
+</script>
+
+<header
+    class="flex justify-between header-background-color max-h-10 p-2 lg:max-h-20 lg:h-20 lg:p-4"
+>
+    <div
+        class="lg:hidden w-10 hover:cursor-pointer rounded-full hover:bg-[#d0d9ba] transition-colors ease-in-out delay-75
+        active:ring-1 active:ring-[#38302E]"
+    >
+        <MdMenu />
+    </div>
+    <div class="w-16 justify-self-center h-full lg:w-32">
+        <img alt="MeetApp logo" src="/logo.png" width="360" height="151" class="h-full" />
+    </div>
+    <div class="w-10" />
+</header>
+
+<style>
+    .header-burger-bg {
+        background-color: var(--tea);
+    }
+
+    .header-background-color {
+        background-color: var(--grass);
+    }
+</style>
--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+    import Header from '../lib/Header/Header.svelte';
+</script>
+
+<Header />
 <h2>index</h2>
 <a href="/users" target="_self">click link</a>
