d6ef6cbb7275353108bed08b6ce0d06fa44e1cf9
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-05 16:46:11 +0100

    Implemented home page, fixed height scaling

--- frontend/src/lib/Header/Header.svelte
+++ frontend/src/lib/Header/Header.svelte
@@ -2,15 +2,15 @@
     import MdMenu from 'svelte-icons/md/MdMenu.svelte';
 </script>
 
-<header class="flex justify-between bg-grass max-h-10 p-2 lg:max-h-20 lg:h-20 lg:p-4">
+<header class="flex justify-between bg-grass p-2 h-[10%] lg:max-h-20 lg:h-20 lg:p-4 align-middle">
     <div
-        class="lg:hidden w-10 hover:cursor-pointer rounded-full hover:bg-tea transition-colors ease-in-out delay-75
+        class="lg:hidden p-1 hover:cursor-pointer rounded-full hover:bg-tea transition-colors ease-in-out delay-75
         active:ring-1 active:ring-cocoa"
     >
         <MdMenu />
     </div>
-    <div class="w-16 justify-self-center h-full lg:w-32">
-        <img alt="MeetApp logo" src="/logo.png" width="360" height="151" class="h-full" />
+    <div class="justify-self-center h-full lg:w-32">
+        <img alt="MeetApp logo" src="/logo.png" class="h-full" />
     </div>
     <div class="w-10" />
 </header>
