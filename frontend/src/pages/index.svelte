d2c6b806a61d9122b3f935e6771d5f6ac5044a90
KyattPL <siema1234@gazeta.pl>
2022-11-05 13:55:26 +0100

    [MEET-52] Add the top banner with logo component (#19)
    * Implemented responsive header
    
    * Removed useless css class
    
    * Added custom colors to tw palette, removed magic hexes

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import Header from '../lib/Header/Header.svelte';
     import Button from '../lib/Button/Button.svelte';
 </script>
 
+<Header />
 <h2>index</h2>
 <a href="/users" target="_self">click link</a>
 
