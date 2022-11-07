1a9867e94a6e3cee509f835ec4ccadcb7bc1b9d4
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-07 22:44:08 +0100

    Changed frontend components to handle new picture type

--- frontend/src/lib/Announcements/AnnouncementListElem/AnnouncementListElem.svelte
+++ frontend/src/lib/Announcements/AnnouncementListElem/AnnouncementListElem.svelte
@@ -1,33 +1,40 @@
 <script lang="ts">
+    import { slide } from 'svelte/transition';
+
     import FaMapMarkedAlt from 'svelte-icons/fa/FaMapMarkedAlt.svelte';
     import Button from '../../../lib/Button/Button.svelte';
 
     export let data;
     export let areDetailsShown: boolean;
+    export let clickHandler: () => void;
 </script>
 
-<div class="bg-olive rounded-2xl m-2 p-2 hover:cursor-pointer">
+<div class="bg-olive rounded-2xl m-2 p-2">
     <div class="flex flex-col">
-        <div class="font-bold">
-            {data.title}
-        </div>
-        <div class="flex flex-row">
-            <div class="w-6 mx-2">
-                <FaMapMarkedAlt />
+        <button on:click={clickHandler} class="hover:cursor-pointer">
+            <div class="font-bold text-left">
+                {data.title}
             </div>
-            {data.location.city.name}
-        </div>
+            <div class="flex flex-row">
+                <div class="w-6 mx-2">
+                    <FaMapMarkedAlt />
+                </div>
+                {data.location.city.name}
+            </div>
+        </button>
         {#if areDetailsShown}
-            <div>
+            <div transition:slide class="border-t-2">
                 {data.description}
             </div>
-            <div class="text-lg">
-                <img src="data:image/jpeg;charset=utf-8;base64, {data.author.profilePicture}" alt="Profile avatar" />
+            <div class="text-lg flex flex-row items-center" in:slide={{ delay: 100 }} out:slide>
+                <div class="w-12 mr-2">
+                    <img class="rounded-full" src={data.author.profilePicture} alt="Profile avatar" />
+                </div>
                 {data.author.firstName}
                 {data.author.lastName}
             </div>
-            <div class="self-center my-2">
-                <Button marginX={12} marginY={2} textSize={20} clickHandler={() => null}>Zapisuję się!</Button>
+            <div class="self-center my-2" in:slide={{ delay: 100 }} out:slide>
+                <Button class="text-base px-12 py-1 mx-12 my-2" clickHandler={() => null}>Zapisuję się!</Button>
             </div>
         {/if}
     </div>
--- frontend/src/lib/Button/Button.svelte
+++ frontend/src/lib/Button/Button.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
     export let clickHandler: () => void;
-    export let marginX: number;
-    export let marginY: number;
-    export let textSize: number;
+    let cssClass: string;
+    export { cssClass as class };
 </script>
 
 <button
     class="bg-pickle border-pine border-2 text-ivory
-        text-[{textSize}px] rounded-full py-{marginY} px-{marginX} hover:opacity-90 transition ease-in-out delay-100
-        focus:ring focus:ring-tea font-bold"
+        rounded-full hover:opacity-90 transition ease-in-out delay-100
+        focus:ring focus:ring-tea font-bold {cssClass}"
     on:click={clickHandler}
 >
     <slot />
