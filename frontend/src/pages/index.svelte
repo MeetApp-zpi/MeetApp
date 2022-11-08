1c5dc37acc44c3bc3bc19667da137efa9a0ad8f7
KyattPL <siema1234@gazeta.pl>
2022-11-08 08:39:49 +0100

    [MEET-63] Add home page (#23)
    * Implemented home page, fixed height scaling
    
    * Fixed header/footer scaling

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,10 +1,46 @@
 <script lang="ts">
+    import FaCalendarDay from 'svelte-icons/fa/FaCalendarDay.svelte';
+    import MdAnnouncement from 'svelte-icons/md/MdAnnouncement.svelte';
+    import MeetingSymbol from '../assets/MeetingSymbol.svelte';
+    import { url } from '@roxi/routify';
+
     import Footer from '../lib/Footer/Footer.svelte';
     import Header from '../lib/Header/Header.svelte';
     import Button from '../lib/Button/Button.svelte';
 </script>
 
-<Header />
-<h2>index</h2>
-<a href="/users" target="_self">click link</a>
-<Footer pageType="main" />
+<div class="h-screen">
+    <Header />
+    <div class="flex flex-col h-[90%] overflow-auto">
+        <a
+            href={$url('/events')}
+            class="p-8 mx-10 my-4 h-1/3 text-pine border-2 rounded-2xl flex flex-col items-center justify-center
+            hover:bg-pine hover:text-ivory transition ease-in-out delay-100"
+        >
+            <div class="w-10">
+                <FaCalendarDay />
+            </div>
+            <div class="text-2xl">Wydarzenia</div>
+        </a>
+        <a
+            href={$url('/announcements')}
+            class="p-8 mx-10 my-4 h-1/3 text-grass border-2 rounded-2xl flex flex-col text-center items-center justify-center
+            hover:bg-grass hover:text-ivory transition ease-in-out delay-100"
+        >
+            <div class="w-10">
+                <MdAnnouncement />
+            </div>
+            <div class="text-2xl">Ogłoszenia</div>
+        </a>
+        <a
+            href={$url('/meetings')}
+            class="p-8 mx-10 my-4 h-1/3 text-pickle border-2 rounded-2xl flex flex-col text-center items-center justify-center
+            hover:bg-pickle hover:text-ivory transition ease-in-out delay-100"
+        >
+            <div class="w-10">
+                <MeetingSymbol />
+            </div>
+            <div class="text-2xl">Spotkania</div>
+        </a>
+    </div>
+</div>
