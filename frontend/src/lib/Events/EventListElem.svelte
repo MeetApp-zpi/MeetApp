502b0d4f2dcad29f0fabee322031818825f7d196
KyattPL <siema1234@gazeta.pl>
2022-11-08 10:06:59 +0100

    Implemented event browser, added missing image file

---
+++ frontend/src/lib/Events/EventListElem.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import FaCalendarAlt from 'svelte-icons/fa/FaCalendarAlt.svelte';
+    import FaMapMarkedAlt from 'svelte-icons/fa/FaMapMarkedAlt.svelte';
+    import MdPeople from 'svelte-icons/md/MdPeople.svelte';
+
+    export let data;
+    export let clickHandler: () => void;
+</script>
+
+<div class="bg-olive rounded-2xl m-2 p-2">
+    <div class="flex flex-row hover:cursor-pointer">
+        <div class="w-1/3 self-center">
+            <img class="w-full" src="no-image.png" alt="Not found" />
+        </div>
+        <div class="flex flex-col w-2/3 pl-2">
+            <button on:click={clickHandler}>
+                <div class="font-bold text-left">
+                    {data.title}
+                </div>
+                <div class="flex flex-row">
+                    <div class="w-6 mx-2">
+                        <FaMapMarkedAlt />
+                    </div>
+                    {data.location.city.name}
+                </div>
+                <div class="flex flex-row">
+                    <div class="w-6 mx-2">
+                        <FaCalendarAlt />
+                    </div>
+                    {data.startDate}
+                </div>
+                <div class="flex flex-row">
+                    <div class="w-6 mx-2">
+                        <MdPeople />
+                    </div>
+                    <div class="text-orange">{data.enrolled} &nbsp;</div>
+                    <div>{data.personQuota !== null ? '/ ' + data.personQuota : ''}</div>
+                </div>
+            </button>
+        </div>
+    </div>
+</div>
--- frontend/src/pages/events/index.svelte
+++ frontend/src/pages/events/index.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
+    import EventListElem from '../../lib/Events/EventListElem.svelte';
     import Footer from '../../lib/Footer/Footer.svelte';
+    import Header from '../../lib/Header/Header.svelte';
+    import execute from '../../lib/fetchWrapper';
+
+    let data = [];
+    let selected: number | null = null;
+
+    execute('events', 'GET')
+        .then((r) => r.json())
+        .then((r) => (data = r));
+
+    const viewDetails = (postId) => {
+        return null;
+    };
 </script>
 
-<Footer pageType="events" />
+<div class="h-screen">
+    <Header />
+    <div class="h-[calc(100%-8rem)] lg:h-[calc(100%-12rem)] overflow-auto">
+        {#each data as item}
+            <EventListElem data={item} clickHandler={() => viewDetails(item.id)} />
+        {/each}
+    </div>
+    <Footer pageType="events" />
+</div>
