ef247ec403240389fa112cafba9431dc6463f611
Mendrejk <bastian.lakomy@gmail.com>
2022-11-09 16:27:26 +0100

    Add LocationController with the GET /locations mapping

---
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/controller/LocationController.java
@@ -0,0 +1,24 @@
+package com.meetapp.meetapp.controller;
+
+import com.meetapp.meetapp.model.Location;
+import com.meetapp.meetapp.service.LocationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class LocationController {
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping("/locations")
+    public List<Location> getLocations() {
+        return locationService.retrieveLocations();
+    }
+}
