d300a9e8976386971af7caf70238846324be067d
Mendrejk <bastian.lakomy@gmail.com>
2022-11-10 18:33:03 +0100

    Add helper constructors to DateTime & Post DTOs

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/DateTimeDTO.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/DateTimeDTO.java
@@ -2,6 +2,13 @@ package com.meetapp.meetapp.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record DateTimeDTO(@NotEmpty String date, //
-                          @NotEmpty String time) {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public record DateTimeDTO(
+        @NotEmpty String date, //
+        String time) {
+    DateTimeDTO(LocalDate date) {
+        this(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")), null);
+    }
 }
--- backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/PostDTO.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/PostDTO.java
@@ -2,11 +2,16 @@ package com.meetapp.meetapp.dto;
 
 import com.meetapp.meetapp.model.Client;
 import com.meetapp.meetapp.model.Location;
+import com.meetapp.meetapp.model.Post;
 import jakarta.validation.constraints.NotNull;
 
 public record PostDTO(@NotNull Integer id, //
                       @NotNull Client author, //
                       @NotNull Location location, //
-                      @NotNull DateTimeDTO creationDateTime, //
+                      @NotNull DateTimeDTO creationDate, //
                       @NotNull Boolean isActive) {
+    public PostDTO(Post post) {
+        this(post.getId(), post.getAuthor(), post.getLocation(), new DateTimeDTO(post.getCreationDate()),
+                post.getIsActive());
+    }
 }
