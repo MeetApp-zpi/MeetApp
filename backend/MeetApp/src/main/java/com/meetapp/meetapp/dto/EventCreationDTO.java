dd996bf464a3561c0b0e16ee1775e58779a04811
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-12 01:22:24 +0100

    Created structure for picture handling in events

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/EventCreationDTO.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/dto/EventCreationDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Set;
 
@@ -36,4 +37,6 @@ public class EventCreationDTO {
 
     @Size(min = 0, max = 5_000)
     private String schedule;
+
+    private MultipartFile picture;
 }
--- backend/MeetApp/src/main/java/com/meetapp/meetapp/service/EventService.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/service/EventService.java
@@ -14,7 +14,12 @@ import com.meetapp.meetapp.security.SessionManager;
 import jakarta.servlet.http.HttpSession;
 import lombok.val;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.format.DateTimeParseException;
 import java.util.List;
@@ -37,14 +42,16 @@ public class EventService {
         return eventRepository.findAll().stream()
                 .map((Event event) -> new EventDTO(new PostDTO(event), event.getTitle(), event.getDescription(),
                         event.getEnrolled(), event.getPersonQuota(), event.getSchedule(),
-                        new DateTimeDTO(event.getStartDate()), new DateTimeDTO(event.getEndDate()))).toList();
+                        new DateTimeDTO(event.getStartDate()), new DateTimeDTO(event.getEndDate()),
+                        event.getPicture())).toList();
     }
 
     public EventDTO retrieveEvent(Integer eventId) {
         val foundEvent = findEventOrThrow(eventId);
         return new EventDTO(new PostDTO(foundEvent), foundEvent.getTitle(), foundEvent.getDescription(),
                 foundEvent.getEnrolled(), foundEvent.getPersonQuota(), foundEvent.getSchedule(),
-                new DateTimeDTO(foundEvent.getStartDate()), new DateTimeDTO(foundEvent.getEndDate()));
+                new DateTimeDTO(foundEvent.getStartDate()), new DateTimeDTO(foundEvent.getEndDate()),
+                foundEvent.getPicture());
     }
 
     public Event createEvent(EventCreationDTO newEvent, HttpSession session) {
@@ -53,10 +60,12 @@ public class EventService {
         Location foundLocation = findLocationOrThrow(newEvent.getLocationId());
         Instant startDate = parseDateOrThrow(newEvent.getStartDate());
         Instant endDate = parseDateOrThrow(newEvent.getEndDate());
+        String pictureUri = newEvent.getPicture() != null ? savePictureAndGetPath(newEvent.getPicture()) : null;
 
         Event eventToSave =
                 new Event(foundClient, foundLocation, newEvent.getTitle(), newEvent.getDescription(), startDate,
                         endDate, newEvent.getPersonQuota(), newEvent.getSchedule());
+        eventToSave.setPicture(pictureUri);
 
         return eventRepository.save(eventToSave);
     }
@@ -68,6 +77,7 @@ public class EventService {
         Instant startDate = parseDateOrThrow(updatedEvent.getStartDate());
         Instant endDate = parseDateOrThrow(updatedEvent.getEndDate());
         Event foundEvent = findEventOrThrow(eventId);
+        String pictureUri = updatedEvent.getPicture() != null ? savePictureAndGetPath(updatedEvent.getPicture()) : null;
 
         if (foundEvent.getAuthor().equals(supposedAuthor)) {
             foundEvent.setDescription(updatedEvent.getDescription());
@@ -78,6 +88,7 @@ public class EventService {
             foundEvent.setPersonQuota(updatedEvent.getPersonQuota());
             foundEvent.setLocation(foundLocation);
             foundEvent.setIsActive(true);
+            foundEvent.setPicture(pictureUri);
 
             return eventRepository.save(foundEvent);
         } else {
@@ -93,6 +104,18 @@ public class EventService {
 
         if (eventToDelete.getAuthor().equals(supposedAuthor)) {
             eventRepository.deleteById(eventId);
+        }
+    }
+
+    public String savePictureAndGetPath(MultipartFile picture) {
+        try {
+            // TODO: how do we set up our paths?
+            Path fileNameAndPath = Paths.get("/static", picture.getOriginalFilename());
+            Files.write(fileNameAndPath, picture.getBytes());
+            return fileNameAndPath.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Event photo with name: " + picture.getOriginalFilename() +
+                    " could not be saved.");
         }
     }
 
