97c6509e0b31cee75c5eaa76a6953b412c346e1a
KyattPL <siema1234@gazeta.pl>
2022-11-21 20:55:24 +0100

    Moved enrolled/enrollees to the Post model

---
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.meetapp.meetapp.repository;
+
+import com.meetapp.meetapp.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
---
+++ frontend/src/pages/enrolled/[postId].svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { goto } from '@roxi/routify';
+
+    import Header from '../../lib/Header/Header.svelte';
+    import execute from '../../lib/fetchWrapper';
+
+    export let postId: number;
+
+    execute(`users/isAuthor/${postId}`, 'GET')
+        .then((r) => r.text())
+        .then((r) => (r === 'false' ? $goto('/') : null));
+</script>
+
+<div class="h-screen">
+    <Header />
+</div>
