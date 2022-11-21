12d8047557d817ad7719669ecfe9446d61a4c6c3
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-21 22:21:02 +0100

    Changed redirect to goto everywhere, finished enrolled list browser

--- frontend/src/pages/enrolled/[postId].svelte
+++ frontend/src/pages/enrolled/[postId].svelte
@@ -2,15 +2,31 @@
     import { goto } from '@roxi/routify';
 
     import Header from '../../lib/Header/Header.svelte';
+    import UserPill from '../../lib/UserPill/UserPill.svelte';
     import execute from '../../lib/fetchWrapper';
 
     export let postId: number;
 
-    execute(`users/isAuthor/${postId}`, 'GET')
+    let users = [];
+
+    let promise = execute(`users/isAuthor/${postId}`, 'GET')
         .then((r) => r.text())
         .then((r) => (r === 'false' ? $goto('/') : null));
+
+    promise = promise.then((_) =>
+        execute(`enrollees/${postId}`, 'GET')
+            .then((r) => r.json())
+            .then((r) => (users = r))
+    );
+
+    $: console.log(users);
 </script>
 
 <div class="h-screen">
     <Header />
+    {#await promise then _}
+        {#each users as user}
+            <UserPill data={user} />
+        {/each}
+    {/await}
 </div>
