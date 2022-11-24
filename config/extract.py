ba147c02bfb904a6d18e5c9bccad09fefae1da62
Kajetan Pynka <siema1234@gazeta.pl>
2022-11-24 01:25:38 +0100

    Importing locations works, need to update svelecte

---
+++ config/extract.py
@@ -0,0 +1,40 @@
+import csv
+
+if __name__ == "__main__":
+    records = []
+    voivodeships = set()
+    cities = set()
+
+    with open("countryExtract.tsv", encoding='UTF-8') as f:
+        records = f.readlines()
+
+    # cities_voivodeships = set()
+    # for record in records[1:]:
+    #     splitted = record.split('\t')
+    #     if splitted[5] != '':
+    #         cities_voivodeships.add(
+    #             splitted[5] + "," + splitted[6].strip().split(' ')[1])
+
+    newLines = []
+    cities_voivodeships = set()
+    for record in records[1:]:
+        splitted = record.split('\t')
+        if splitted[5] != '':
+            voivodeship = splitted[6].strip().split(' ')[1]
+            testPair = splitted[5] + "," + voivodeship
+            if testPair not in cities_voivodeships:
+                newRecord = []
+                newRecord.append(splitted[5])
+                newRecord.append(voivodeship)
+                newRecord.append(splitted[2])
+                newRecord.append(splitted[1])
+                newLines.append(newRecord)
+                cities_voivodeships.add(testPair)
+
+    for re in newLines:
+        if ',' in re[3]:
+            print(re)
+
+    with open("cities.csv", "w", newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(newLines)
