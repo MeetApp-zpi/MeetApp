0b07d1228ff73d1ac0c7c812ca0b33aab17c9669
Deamacto <pszon.joanna@gmail.com>
2022-11-28 14:06:04 +0100

    changed styling of inputs

--- frontend/src/lib/MultiselectInput/MultiselectInput.svelte
+++ frontend/src/lib/MultiselectInput/MultiselectInput.svelte
@@ -8,12 +8,28 @@
     export let style;
 </script>
 
-<div class="svelecte-multiselect">
+<div class="svelecte-multiselect categorySvelecteControl categorySvelecteContent">
     <Svelecte {style} options={data} {placeholder} {inputId} multiple="true" bind:value={selected} />
 </div>
 
 <style>
     .svelecte-multiselect :global(.sv-content) {
         overflow-x: scroll;
+    }
+
+    .categorySvelecteControl :global(.sv-control) {
+        border-color: var(--pickle);
+        background-color: var(--ivory);
+        border-width: 2px;
+        padding: 0.5rem 0.5rem 0.5rem 0.5rem;
+        color: var(--gray);
+        border-radius: 0.5rem;
+        font-size: 1.5rem;
+        /* line-height: 2rem; */
+    }
+
+    .categorySvelecteContent :global(.inputBox) {
+        height: 2rem;
+        color: var(--gray);
     }
 </style>
