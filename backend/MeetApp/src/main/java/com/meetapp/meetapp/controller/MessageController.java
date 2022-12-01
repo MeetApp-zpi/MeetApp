fe01fb2cf0b23125ecfbc5801a7342828c6a3475
Kajetan Pynka <siema1234@gazeta.pl>
2022-12-01 15:09:09 +0100

    Implemented basic chat functionality

--- backend/MeetApp/src/main/java/com/meetapp/meetapp/controller/MessageController.java
+++ backend/MeetApp/src/main/java/com/meetapp/meetapp/controller/MessageController.java
@@ -3,6 +3,7 @@ package com.meetapp.meetapp.controller;
 import com.meetapp.meetapp.model.Message;
 import com.meetapp.meetapp.service.MessageService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +22,11 @@ public class MessageController {
     public List<Message> retrieveChatroomMessages(@PathVariable Integer chatroomId, @RequestParam Integer page,
                                                   HttpSession session) {
         return messageService.retrieveMessages(chatroomId, session, page);
+    }
+
+    @PostMapping("/messages/{chatroomId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Message createMessage(HttpSession session, @PathVariable Integer chatroomId, @RequestBody String content) {
+        return messageService.createMessage(chatroomId, session, content);
     }
 }
--- frontend/src/pages/chat/[chatroomId]/index.svelte
+++ frontend/src/pages/chat/[chatroomId]/index.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
     import io from 'socket.io-client';
     import { goto } from '@roxi/routify';
+    import { onMount } from 'svelte';
+
     import execute from '../../../lib/fetchWrapper';
 
     export let chatroomId: number;
 
     let chatInputValue;
     let chatMessages = [];
+    let page: number = 0;
+    let messagesContainer;
+    let isScrolled: boolean = false;
 
     let promise = execute(`chatrooms/existsById/${chatroomId}`, 'GET')
         .then((r) => (r.status === 500 ? $goto('/') : r.json()))
         .then((r) => (r === false ? $goto('/') : null))
         .catch((err) => $goto('/'));
 
-    promise = promise.then((_) => execute(`chatrooms`));
+    const retrieveMessages = (pageNum: number) => {
+        execute(`messages/${chatroomId}?page=${pageNum}`, 'GET')
+            .then((r) => r.json())
+            .then((r) => {
+                r.reverse();
+                chatMessages = [...r, ...chatMessages];
+            });
+    };
+
+    const scrollToBottom = (node) => {
+        node.scroll({
+            top: node.scrollHeight,
+            behavior: 'smooth'
+        });
+    };
+
+    const infiniteScroll = () => {
+        const messagesContainer = document.getElementById('messagesContainer');
+        if (messagesContainer.scrollTop === 0) {
+            page = page + 1;
+        }
+    };
+
+    const updateScroll = () => {
+        setTimeout(() => scrollToBottom(messagesContainer), 1000);
+    };
+
+    onMount(() => {
+        updateScroll();
+    });
 
     const socket = io('http://localhost:3000', {
         path: '/websockets',
@@ -26,15 +60,34 @@
 
     const sendMessage = () => {
         if (chatInputValue.length > 0) {
+            execute(`messages/${chatroomId}`, 'POST', chatInputValue).then((r) => (r.status === 201 ? addNewMessage(r.json()) : null));
+        }
+    };
+
+    const addNewMessage = async (msg) => {
+        msg.then((r) => {
+            chatMessages = [...chatMessages, r];
             socket.emit('priv msg', {
-                content: chatInputValue,
+                content: r,
                 to: chatroomId
             });
+        });
+    };
+
+    $: {
+        retrieveMessages(page);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage();
+            chatInputValue = '';
+            updateScroll();
         }
     };
 
     socket.on('priv msg', ({ content, from }) => {
-        console.log(from, content);
+        chatMessages = [...chatMessages, content];
     });
 </script>
 
@@ -42,7 +95,12 @@
     <script src="https://cdn.socket.io/4.5.4/socket.io.min.js"></script>
 </svelte:head>
 
-{#await promise then _}
-    <input type="text" bind:value={chatInputValue} />
-    <button class="h-10 w-10" on:click={sendMessage}>Send message!</button>
-{/await}
+<div class="h-40 overflow-auto" on:scroll={infiniteScroll} id="messagesContainer" bind:this={messagesContainer}>
+    {#await promise then _}
+        {#each chatMessages as chatMessage}
+            <div class="h-6">{chatMessage.content}</div>
+        {/each}
+    {/await}
+</div>
+<input type="text" bind:value={chatInputValue} on:keydown={handleKeyDown} />
+<button class="h-10 w-10" on:click={sendMessage}>Send message!</button>
