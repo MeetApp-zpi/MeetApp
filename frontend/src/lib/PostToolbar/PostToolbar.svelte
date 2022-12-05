c789d32d2bedf932311620a93b5ed5f261174177
KyattPL <siema1234@gazeta.pl>
2022-12-05 18:46:58 +0100

    Fixed missing pointer cursor on clickable elements (#69)

--- frontend/src/lib/PostToolbar/PostToolbar.svelte
+++ frontend/src/lib/PostToolbar/PostToolbar.svelte
@@ -33,29 +33,29 @@
 
 <div class="flex flex-row -mt-4 justify-end mr-4">
     <div
-        class="bg-olive w-10 h-10 rounded-b-lg p-2 text-cocoa"
+        class="bg-olive w-10 h-10 rounded-b-lg p-2 text-cocoa hover:cursor-pointer"
         on:click={() => $goto(`/enrolled/${postId}`)}
         on:keydown={() => $goto(`/enrolled/${postId}`)}
     >
         <FaUserFriends />
     </div>
     <div
-        class="bg-olive mx-4 w-10 h-10 rounded-b-lg p-2 text-cocoa"
+        class="bg-olive mx-4 w-10 h-10 rounded-b-lg p-2 text-cocoa hover:cursor-pointer"
         on:click={() => $goto(`/${postType}/edit/${postId}`)}
         on:keydown={() => $goto(`/${postType}/edit/${postId}`)}
     >
         <MdEdit />
     </div>
     {#if isPostActive}
-        <div class="bg-olive mr-4 w-10 h-10 rounded-b-lg p-2 text-cocoa" on:click={deactivate} on:keydown={deactivate}>
+        <div class="bg-olive mr-4 w-10 h-10 rounded-b-lg p-2 text-cocoa hover:cursor-pointer" on:click={deactivate} on:keydown={deactivate}>
             <FaUserSlash />
         </div>
     {:else}
-        <div class="bg-olive mr-4 w-10 h-10 rounded-b-lg p-2 text-cocoa" on:click={activate} on:keydown={activate}>
+        <div class="bg-olive mr-4 w-10 h-10 rounded-b-lg p-2 text-cocoa hover:cursor-pointer" on:click={activate} on:keydown={activate}>
             <FaUserCheck />
         </div>
     {/if}
-    <div class="bg-olive w-10 h-10 rounded-b-lg p-2 text-cocoa" on:click={deletePost} on:keydown={deletePost}>
+    <div class="bg-olive w-10 h-10 rounded-b-lg p-2 text-cocoa hover:cursor-pointer" on:click={deletePost} on:keydown={deletePost}>
         <MdDelete />
     </div>
 </div>
