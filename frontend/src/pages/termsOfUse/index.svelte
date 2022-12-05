d0d1a2f632f064f3b506aefcd662a1a9ba6815fe
KyattPL <siema1234@gazeta.pl>
2022-12-05 18:48:51 +0100

    [MEET-113] Create terms of use page (#64)
    * Added terms of use page
    
    * Added terms of use to the burger menu

---
+++ frontend/src/pages/termsOfUse/index.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import Header from '../../lib/Header/Header.svelte';
+</script>
+
+<div class="h-screen">
+    <Header />
+    <div class="h-[calc(100%-4rem)] lg:h-[calc(100%-8rem)] overflow-auto p-4">
+        <h1 class="text-3xl text-cocoa mb-2">Zasady MeetApp</h1>
+        <p class="text-base text-cocoa mb-2">
+            Celem MeetApp jest stworzenie globalnej przestrzeni do rozmowy. Przemoc, nękanie i podobne zachowania zniechęcają ludzi do wyrażania
+            siebie i w rezultacie zmniejszają wartość globalnej publicznej rozmowy. Nasze Zasady mają zapewnić naszym użytkownikom swobodny i
+            bezpieczny udział w publicznej rozmowie.
+        </p>
+        <h2 class="text-2xl text-cocoa mb-2">Bezpieczeństwo</h2>
+        <p class="text-base text-cocoa mb-2">
+            Przemoc: niedozwolone jest grożenie użyciem przemocy żadnej osobie ani grupie osób. Niedozwolone jest również wyrażanie pochwały dla
+            przemocy. Terroryzm / brutalny ekstremizm: niedozwolone jest grożenie aktami terroryzmu lub brutalnego ekstremizmu ani ich promowanie.
+            Dziecięca pornografia: na MeetApp niedozwolone jest udostępnianie dziecięcej pornografii pod żadną postacią. Nadużycia/nękanie:
+            niedozwolone jest angażowanie się w ukierunkowane nękanie innych osób ani nakłanianie kogokolwiek do takich działań. Obejmuje to wyrażanie
+            pragnienia lub życzenia by ktoś doświadczył fizycznej krzywdy. Zachowanie nacechowane nienawiścią: niedozwolone jest promowanie przemocy
+            wobec innych, grożenie im ani ich nękanie z powodu rasy, przynależności etnicznej, kasty, pochodzenia, orientacji seksualnej, płci,
+            tożsamości płciowej, religii, wieku, niepełnosprawności czy choroby. Osoby odpowiedzialne za brutalne akty przemocy: konta należące do
+            osób odpowiedzialnych za ataki terrorystyczne lub inne brutalne i masowe akty przemocy na tle ideologicznym zostaną usunięte. Dotyczy to
+            również postów wyrażających radykalne poglądy tych osób i innych utworzonych przez nich treści. Samobójstwo lub samookaleczenie:
+            niedozwolone jest promowanie samobójstwa lub samookaleczenia ani zachęcanie do tych czynów. Nieodpowiednie treści, w tym przemoc graficzna
+            i treści dla dorosłych: niedozwolone jest publikowanie nadmiernie brutalnych multimediów ani udostępnianie treści przepełnionych przemocą
+            lub treści dla dorosłych w ramach transmisji na żywo, w zdjęciach profilowych lub nagłówkach. Zabrania się również udostępniania
+            multimediów przedstawiających przemoc/napaść seksualną. Nielegalne lub regulowane prawnie towary lub usługi: niedozwolone jest korzystanie
+            z naszej usługi w jakimkolwiek niezgodnym z prawem celu lub w celu wspierania nielegalnych aktywności. Obejmuje to sprzedaż, kupowanie lub
+            ułatwianie transakcji dotyczących nielegalnych towarów lub usług, a także niektórych rodzajów regulowanych towarów lub usług.
+        </p>
+        <h2 class="text-2xl text-cocoa mb-2">Prywatność</h2>
+        <p class="text-base text-cocoa mb-2">
+            Informacje osobiste: niedozwolone jest ujawnianie ani publikowanie informacji osobistych innych osób (takich jak prywatny numer telefonu i
+            adres) bez ich upoważnienia i wyraźnej zgody. Zabrania się również grożenia ujawnieniem informacji osobistych lub zachęcania innych do
+            tego. Prywatne materiały: niedozwolone jest publikowanie czy udostępnianie prywatnych zdjęć lub filmów przedstawiających osobę trzecią,
+            które zostały utworzone lub rozpowszechnione bez jej wyraźnej zgody.
+        </p>
+        <h2 class="text-2xl text-cocoa mb-2">Autentyczność</h2>
+        <p class="text-base text-cocoa mb-2">
+            Manipulowanie platformą MeetApp i spam: niedozwolone jest korzystanie z usług MeetApp w sposób sztucznie zwiększający lub ograniczający
+            przepływ informacji ani angażowanie się w zachowania, które modyfikują lub zakłócają sposób korzystania z MeetApp przez innych
+            użytkowników. Integralność obywatelska: niedozwolone jest korzystanie z usług MeetApp w celu wpływania na wybory czy inne procesy
+            obywatelskie lub ich zakłócania. Obejmuje to publikowanie lub udostępnianie treści, które mogą uniemożliwić uczestniczenie w procesie
+            obywatelskim lub wprowadzać w błąd co do tego, kiedy, gdzie lub w jaki sposób można w nim uczestniczyć. Wprowadzanie innych użytkowników w
+            błąd co do swojej tożsamości: niedozwolone jest podszywanie się pod inne osoby, grupy lub organizacje w celu wprowadzenia w błąd,
+            zwiedzenia lub oszukania innych, w tym posługiwanie się fałszywą tożsamością w sposób utrudniający innym korzystanie z MeetApp.
+            Nieautentyczne i edytowane multimedia: niedozwolone jest udostępnianie nieautentycznych czy edytowanych multimediów, których treść mogłaby
+            komuś zaszkodzić, bez wyraźnego oznaczenia ich rodzaju. Ponadto możemy oznaczać posty zawierające edytowane i nieautentyczne multimedia,
+            aby pomóc użytkownikom zrozumieć ich pochodzenie i zapewnić dodatkowy kontekst. Prawa autorskie i znaki towarowe: niedozwolone jest
+            naruszanie praw własności intelektualnej innych osób, w tym praw autorskich i znaków towarowych.
+        </p>
+        <h2 class="text-2xl text-cocoa mb-2">Reklamy stron trzecich zawarte w treściach wideo</h2>
+        <p class="text-base text-cocoa mb-2">
+            Nie wolno przesyłać, udostępniać ani wyświetlać w naszych serwisach żadnych treści wideo zawierających reklamy stron trzecich, takie jak
+            reklamy w trybie „pre-roll” lub sponsorowane treści graficzne, bez naszej uprzedniej zgody.
+        </p>
+    </div>
+</div>
